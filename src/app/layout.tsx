e2855ccde8397c4a07c3b560d4d4643dcfb9bb91
joshuasilva414 <joshuasilva414@gmail.com>
2024-11-17 04:08:38 -0600

    custom signin/signup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,39 @@
 import "@/styles/globals.css";
 
 import {
-	ClerkProvider,
-	SignInButton,
-	SignedIn,
-	SignedOut,
-	UserButton,
+    ClerkProvider,
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
 } from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "People Build Things",
-	description: "Building things is hard, but you got this :)",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
+    title: "People Build Things",
+    description: "Building things is hard, but you got this :)",
+    icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export default function RootLayout({
-	children,
-}: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<ClerkProvider>
-			<html lang="en" className={`${GeistSans.variable}`}>
-				<body>
-					<SignedOut>
-						<SignInButton />
-					</SignedOut>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
-					{children}
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+    children,
+}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <ClerkProvider>
+            <html lang="en" className={`${GeistSans.variable}`}>
+                <body>
+                    <div className="flex justify-end p-4"><SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn></div>
+
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
