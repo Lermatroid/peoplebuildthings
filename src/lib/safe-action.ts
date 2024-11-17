f6b5e87782942a236e135ab0a64eea85eb74af58
joshuasilva414 <joshuasilva414@gmail.com>
2024-11-17 01:49:45 -0600

    Add basic user signup/signin

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,15 +1,18 @@
-import { createSafeActionClient, returnValidationErrors } from "next-safe-action";
+import {
+	createSafeActionClient,
+	returnValidationErrors,
+} from "next-safe-action";
 import { z } from "zod";
 import { auth } from "@clerk/nextjs/server";
 
 export const publicAction = createSafeActionClient();
 
-export const protectedAction = publicAction.use(async ({next, ctx}) => {
-    const { userId } = await auth();
-		if (!userId)
-			returnValidationErrors(z.null(), {
-				_errors: ["Unauthorized (No User ID)"],
-			});
-		// TODO: add check for registration
-		return next({ ctx: { userId } });
+export const protectedAction = publicAction.use(async ({ next, ctx }) => {
+	const { userId } = await auth();
+	if (!userId)
+		returnValidationErrors(z.null(), {
+			_errors: ["Unauthorized (No User ID)"],
+		});
+	// TODO: add check for registration
+	return next({ ctx: { userId } });
 });
